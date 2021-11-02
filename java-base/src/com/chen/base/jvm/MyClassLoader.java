7468e1261215952b3c1da0dd3e428b26d629ea33
chenkun <462488588@qq.com>
2021-11-02 11:44:02 +0800

    feat(study): 添加自定义类加载器测试

---
+++ java-base/src/com/chen/base/jvm/MyClassLoader.java
@@ -0,0 +1,40 @@
+package com.chen.base.jvm;
+
+import java.util.Base64;
+
+/**
+ * @author chenkun
+ * @Description 自定义类加载器
+ * @date 2021/11/2 上午11:42
+ */
+public class MyClassLoader extends ClassLoader {
+    public static void main(String[] args) {
+        try {
+            new MyClassLoader().findClass("jvm.Hello").newInstance(); // 加载并初始化Hello类
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        String helloBase64 = "yv66vgAAADQAHwoABgARCQASABMIABQKABUAFgcAFwcAGAEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBABJMb2N" +
+                "hbFZhcmlhYmxlVGFibGUBAAR0aGlzAQALTGp2bS9IZWxsbzsBAAg8Y2xpbml0PgEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABkMABoAGwEAGEhlb" +
+                "GxvIENsYXNzIEluaXRpYWxpemVkIQcAHAwAHQAeAQAJanZtL0hlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2" +
+                "YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACA" +
+                "ABAAkAAAAvAAEAAQAAAAUqtwABsQAAAAIACgAAAAYAAQAAAAMACwAAAAwAAQAAAAUADAANAAAACAAOAAgAAQAJAAAAJQACAAAAAAAJsgACEgO2AASxAAAAAQAK" +
+                "AAAACgACAAAABgAIAAcAAQAPAAAAAgAQ";
+
+        byte[] bytes = decode(helloBase64);
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    public byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+}
